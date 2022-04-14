2bb4c8e3ea5ceede67229eb15a281281ca413a03
Daniel Erazo <69876673+Danii2020@users.noreply.github.com>
2022-04-14 12:20:00 -0500

    Delete pass-hash.ts

--- pass-hash.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-async function hashPassword() {
-  const myPassword = 'admin123';
-  const hash = await bcrypt.hash(myPassword, 10);
-  console.log(hash);
-}
-
-hashPassword();
