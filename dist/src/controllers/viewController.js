5fe48e63f5b29aea4928c0e90acbd34f373e2253
Danii2020 <danielmauricioerazoespinoza@gmail.com>
2022-04-22 13:36:32 -0500

    Logout functionality added

--- dist/src/controllers/viewController.js
+++ dist/src/controllers/viewController.js
@@ -1,37 +1,32 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const boom_1 = __importDefault(require("@hapi/boom"));
-const prisma = new client_1.PrismaClient();
-class ViewController {
-    static async getSignUp(req, res, next) {
-        try {
-            res.render("auth/signup");
-        }
-        catch (error) {
-            console.log(error);
-            next(boom_1.default.internal("Internal server error"));
-        }
-    }
-    static async getLogin(req, res, next) {
-        try {
-            res.render("auth/login");
-        }
-        catch (error) {
-            console.log(error);
-            next(boom_1.default.internal("Internal server error"));
-        }
-    }
-    static async getNewPost(req, res, next) {
-        try {
-            res.render("posts/newPost");
-        }
-        catch (error) {
-            next(boom_1.default.internal("Internal server error"));
-        }
-    }
-}
-exports.default = ViewController;
+// import { PrismaClient } from '@prisma/client';
+// import { Request, Response, NextFunction } from 'express';
+// import { IPost, IUser } from '../models/interfaces';
+// import boom from '@hapi/boom';
+// const prisma = new PrismaClient();
+// class ViewController {
+//   public static async getSignUp(req:Request, res:Response, next:NextFunction):Promise<any> {
+//     try {
+//       res.render("auth/signup");
+//     } catch (error) {
+//       console.log(error);
+//       next(boom.internal("Internal server error"))
+//     }
+//   }
+//   public static async getLogin(req:Request, res:Response, next:NextFunction):Promise<any> {
+//     try {
+//       res.render("auth/login");
+//     } catch (error) {
+//       console.log(error);
+//       next(boom.internal("Internal server error"))
+//     }
+//   }
+//   public static async getNewPost(req:Request, res:Response, next:NextFunction):Promise<any> {
+//     try {
+//       res.render("posts/newPost");
+//     } catch (error) {
+//       next(boom.internal("Internal server error"));
+//     }
+//   }
+// }
+// export default ViewController;
--- src/controllers/viewController.ts
+++ src/controllers/viewController.ts
@@ -1,35 +1,35 @@
-import { PrismaClient } from '@prisma/client';
-import { Request, Response, NextFunction } from 'express';
-import { IPost, IUser } from '../models/interfaces';
-import boom from '@hapi/boom';
+// import { PrismaClient } from '@prisma/client';
+// import { Request, Response, NextFunction } from 'express';
+// import { IPost, IUser } from '../models/interfaces';
+// import boom from '@hapi/boom';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-class ViewController {
-  public static async getSignUp(req:Request, res:Response, next:NextFunction):Promise<any> {
-    try {
-      res.render("auth/signup");
-    } catch (error) {
-      console.log(error);
-      next(boom.internal("Internal server error"))
-    }
-  }
+// class ViewController {
+//   public static async getSignUp(req:Request, res:Response, next:NextFunction):Promise<any> {
+//     try {
+//       res.render("auth/signup");
+//     } catch (error) {
+//       console.log(error);
+//       next(boom.internal("Internal server error"))
+//     }
+//   }
 
-  public static async getLogin(req:Request, res:Response, next:NextFunction):Promise<any> {
-    try {
-      res.render("auth/login");
-    } catch (error) {
-      console.log(error);
-      next(boom.internal("Internal server error"))
-    }
-  }
-  public static async getNewPost(req:Request, res:Response, next:NextFunction):Promise<any> {
-    try {
-      res.render("posts/newPost");
-    } catch (error) {
-      next(boom.internal("Internal server error"));
-    }
-  }
-}
+//   public static async getLogin(req:Request, res:Response, next:NextFunction):Promise<any> {
+//     try {
+//       res.render("auth/login");
+//     } catch (error) {
+//       console.log(error);
+//       next(boom.internal("Internal server error"))
+//     }
+//   }
+//   public static async getNewPost(req:Request, res:Response, next:NextFunction):Promise<any> {
+//     try {
+//       res.render("posts/newPost");
+//     } catch (error) {
+//       next(boom.internal("Internal server error"));
+//     }
+//   }
+// }
 
-export default ViewController;
+// export default ViewController;
