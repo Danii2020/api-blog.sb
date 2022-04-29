21e83c40ed16e771957e145d9927044cccbefef1
Danii2020 <danielmauricioerazoespinoza@gmail.com>
2022-04-29 12:51:41 -0500

    Cypress added

---
+++ dist/cypress/integration/login.spec.js
@@ -0,0 +1,19 @@
+"use strict";
+describe("Test login view", () => {
+    context("when the login page is visited", () => {
+        it("logins a user with email and password", () => {
+            cy.visit('http://localhost:3000/view/auth/login');
+            cy.wait(10);
+            cy.get("[id=email]")
+                .type("daniel@gmail.com");
+            cy.wait(10);
+            cy.get("[id=password]")
+                .type("daniel123");
+            cy.wait(10);
+            cy.get("[id=login-button]")
+                .click();
+            cy.url()
+                .should("include", "/profile");
+        });
+    });
+});
