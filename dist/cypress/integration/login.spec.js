f4270888fb5cae6babc18d811c5553aee76dab82
Danii2020 <danielmauricioerazoespinoza@gmail.com>
2022-05-03 15:10:08 -0500

    Cypress testing changed

--- dist/cypress/integration/login.spec.js
+++ dist/cypress/integration/login.spec.js
@@ -1,4 +1,28 @@
 "use strict";
+describe("Test signup view", () => {
+    context("when the signup page is visited", () => {
+        it("logins a user with email and password", () => {
+            cy.visit('http://localhost:3000/view/auth/login');
+            cy.wait(10);
+            cy.get("[id=firstname]")
+                .type("Daniel");
+            cy.wait(10);
+            cy.get("[id=lastname]")
+                .type("Erazo");
+            cy.wait(10);
+            cy.get("[id=username]")
+                .type("danii");
+            cy.get("[id=email]")
+                .type("daniel@gmail.com");
+            cy.get("[id=password]")
+                .type("daniel123");
+            cy.get("[id=signup-button]")
+                .click();
+            cy.url()
+                .should("include", "/login");
+        });
+    });
+});
 describe("Test login view", () => {
     context("when the login page is visited", () => {
         it("logins a user with email and password", () => {
